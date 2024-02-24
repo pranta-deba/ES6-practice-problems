626fa1ad52b161cecd939abd15a2ed0f122a1493
Pranto Deb <rajprit0m@outlook.com>
2024-02-24 16:41:59 +0600

    problems 1 done

---
+++ problem1.js
@@ -0,0 +1,15 @@
+// 1 :
+const multiply = (a,b,c) => a*b*c;
+// console.log(multiply(1,2,3));
+
+
+// 2 :
+const line = `I am a web developer. 
+I love to code. 
+I love to eat biryani.`;
+// console.log(line);
+
+
+// 3 :
+const sum = (a, b=0) => a+b;
+// console.log(sum(2));
